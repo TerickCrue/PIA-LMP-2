93a252dc4a5d8643237c8000f97f48bae896bf2b
Erick <118601533+TerickCrue@users.noreply.github.com>
2024-05-26 04:11:11 -0600

    connection

--- Backend/PIA-LMP-API/Program.cs
+++ Backend/PIA-LMP-API/Program.cs
@@ -1,3 +1,7 @@
+using PIA_LMP_API.Data;
+
+using PIA_LMP_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSqlServer<PIALMPContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 var app = builder.Build();
 
